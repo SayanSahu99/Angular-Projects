dbeede8c1ef89b35dacd9833f1f4fc4e704b5851
SayanSahu99 <sayansovan@gmail.com>
2023-02-01 12:59:30 +0530

    databinding done

--- cmp-databinding-assignment/src/app/app.component.ts
+++ cmp-databinding-assignment/src/app/app.component.ts
@@ -7,10 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cmp-databinding-assignment';
+  oddElements: number[] = [];
+  evenElements: number[] = [];
   interval: number = 0;
 
   getInterval(event: number) {
     console.log(event);
     this.interval = event;
+    if (this.interval % 2 === 0) {
+      this.evenElements.push(this.interval);
+    }
+    else {
+      this.oddElements.push(this.interval);
+    }
   }
 }
--- cmp-databinding-assignment/src/app/even/even.component.ts
+++ cmp-databinding-assignment/src/app/even/even.component.ts
@@ -7,8 +7,7 @@ import { Component, Input } from '@angular/core';
 })
 export class EvenComponent {
 
-  @Input("even")
-  evenNumber!: number;
+  @Input("even") evenNumber!: number;
 
   constructor() { }
 
